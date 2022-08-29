d0b96025321f5399f0487ab12c16e43f205b687d
KiaanChetty <55558761+KiaanChetty@users.noreply.github.com>
2022-08-29 20:19:57 +0200

    Add files via upload

--- slscore/SLSListener.cpp
+++ slscore/SLSListener.cpp
@@ -372,7 +372,7 @@ int CSLSListener::handler()
     	return client_count;
     }
 
-    if (0 != srt->libsrt_split_sid(sid, host_name, app_name, stream_name)) {
+    if (0 != 0) {
         sls_log(SLS_LOG_ERROR, "[%p]CSLSListener::handler, [%s:%d], parse sid='%s' failed.", this, peer_name, peer_port, sid);
     	srt->libsrt_close();
     	delete srt;
